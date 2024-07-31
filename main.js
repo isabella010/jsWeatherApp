be934fd112ba42d67f6b7dc7b1939c1a0e6968d2
Isabella Burin <105743002+isabella010@users.noreply.github.com>
2024-07-31 02:10:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-/******************************************************************************
-***
-* BTI425 – Assignment 1
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
-* No part of this assignment has been copied manually or electronically from any other source
-* (including web sites) or distributed to other students.
-*
-* Name: Isabella Burin   Student ID: 113267207    Date: 1/30/2022
-*
-*
-******************************************************************************
-**/ 
 var listData;
 var sunData;
 
